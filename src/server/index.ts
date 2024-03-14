1defa280ccff970963f771261757533325ec9a8b
yuchanns <me@yuchanns.xyz>
2024-03-14 13:45:14 +0800

    feat(authorize): add authorize middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { createMiddleware } from 'hono/factory'
 import { logger } from 'hono/logger'
 import task from './task'
 import sub from './sub'
@@ -8,10 +9,25 @@ export type Bindings = {
   LOSS_THRESHOLD: number
   DELAY_THRESHOLD: number
   RANDOM_COUNT: number
+  SECRET_KEY: string
 }
 
-const app = new Hono<{ Bindings: Bindings }>().use(logger())
+export const authorize = () => {
+  return createMiddleware<{ Bindings: Bindings }>(async (c, next) => {
+    if (c.env.SECRET_KEY == '') {
+      return await next()
+    }
+    if (c.req.query('token') !== c.env.SECRET_KEY) {
+      return c.newResponse('Unauthorized', 403)
+    }
+    return await next()
+  })
+}
 
+const app = new Hono<{ Bindings: Bindings }>()
+  .use(logger(), authorize())
+
+// TODO: friendly homepage
 app.get('/', (c) => c.text('Hello Warp'))
 
 app.route('/task', task)
